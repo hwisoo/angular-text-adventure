d0062d1ede5138141ed25350224763cd21527a52
James Cho and Kenny Wolfenberger <james@>
2019-01-30 14:51:10 -0800

    more endings

--- src/app/shared/scenes.ts
+++ src/app/shared/scenes.ts
@@ -3,7 +3,7 @@ import { Story } from '../story';
 export const SCENES: any[] = [
     // living room / encounter zombie
     new Story("You are in the living room", "You encounter a zombie. What do you want to do?", ["Run", "Attack", "Call for Help"], [1, 2, 3], 0),
-    
+
     // living room /outcome
     new Story("You run from the zombie and manage to escape to the attic.", "The zombie is following you.", ["Stand and fight.", "Jump out the window."], [2, 4], 0),
     new Story("You attacked the zombie and defeated it, but it did some damage.", "Where do you wanna go next?", ["Kitchen", "Bedroom", "Outside"], [5, 6, 7], 25),
@@ -14,13 +14,13 @@ export const SCENES: any[] = [
     new Story("You wander into the kitchen and find some chicken in the fridge.", "This could restore your health.", ["Eat it", "Move on to the bedroom."], [8, 6], 0),
 
     //bedroom #6
-    new Story("You move to the bedroom. There is nothing interesting here.", "Go outside?", ["yes", "no"], [7, 6], 0),
+    new Story("You move to the bedroom. You hear someone calling for help in the closet.", "Do you Help?", ["Go Help.", "Go outside"], [9, 7], 0),
 
-    //outside
+    //outside #7
     new Story("You go outside and hear a helicopter flying overhead.", "You wonder if they are friendly.", ["Run for cover", "Wave the helicopter down"], [9, 10], 0),
 
     new Story("You eat the chicken and immediately feel worse.", "Where to next?", ["Bedroom", "Outside"], [6, 7], 30),
-    new Story("You eat the chicken and immediately feel worse.", "Where to next?", ["Bedroom", "Outside"], [6, 7], 30),
+    new Story("You go to help and find that a raider has tricked you. He knocks you out unconscious", "He takes your weapon and leaves you to die", ["Start Over"], [0], 100),
     new Story("You flag down the helicopter and get flown to safety.", "You win!", ["Start Over"], [0], 0),
 
 ]
